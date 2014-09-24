acc097a75f027341ab8f883988369c90104c1a7f
thingywhat <thingywhat@live.com>
2014-09-23 22:20:07 -0600

    Initial commit.

---
+++ xvideosDownloader.js
@@ -0,0 +1,14 @@
+/// ==UserScript==
+// @name          xvideos downloader
+// @namespace     xvideos downloader
+// @description	  Allows you to download xvideos without an account
+// @author        thingywhat
+// @include       http://www.xvideos.com/video*/*
+// @version       1
+// @grant         none
+// ==/UserScript==
+
+$('[data-ref=tabDownload]').unbind().click(function(){
+    location.replace(/flv_url=([^\|]+)/.exec(
+	decodeURIComponent($("#flash-player-embed").attr("flashvars")))[1]);
+});
